b61bc9bb45b37ddbbb36858db5c09a3b5bbc7012
Jorge Chan <jorgeluis.chanchan@gmail.com>
2025-04-04 14:24:50 -0600

    Permite corregir que las rutas puedan recibir valores dinámicos.

--- core/Router.php
+++ core/Router.php
@@ -12,26 +12,36 @@
             // Verifica las rutas registradas
             // var_dump($this->routes);
 
-            // echo "URL solicitada: " . $url . "<br />";  // Verifica si el valor de $url es el esperado
-            if (array_key_exists($url, $this->routes)) {
-                $controller = "Src\\Controllers\\" . $this->routes[$url]['controller'];
-                $method = $this->routes[$url]['method'];
+            foreach ($this->routes as $route => $routeDetails){
+                // Compara la ruta dinámica con una expresión regular
+                $pattern = preg_replace('/{([a-zA-Z0-9_]+)}/', '([a-zA-Z0-9_]+)', $route);
+                $pattern = '#^' . $pattern . '$#';
 
-                if (class_exists($controller)) {
-                    $controllerInstance = new $controller();
-                    if (method_exists($controllerInstance, $method)) {
-                        $controllerInstance->$method();
-                        return;
+                // Si coincide la ruta
+                if (preg_match($pattern, $url, $matches)) {
+                    // Extrae los parámetros de la ruta
+                    array_shift($matches); // El primer valor es el valor completo de la ruta, lo eliminamos
+                    $controller = "Src\\Controllers\\" . $routeDetails['controller'];
+                    $method = $routeDetails['method'];
+
+                    if (class_exists($controller)) {
+                        $controllerInstance = new $controller();
+
+                        if (method_exists($controllerInstance, $method)) {
+                            // Llamamos al método, pasándole los parámetros extraídos
+                            call_user_func_array([$controllerInstance, $method], $matches);
+                            return;
+                        } else {
+                            echo "Método no encontrado: " . $method . "<br />";
+                        }
                     } else {
-                        echo "Método no encontrado: " . $method . "<br />";
+                        echo "Controlador no encontrado: " . $controller . "<br />";
                     }
-                } else {
-                    echo "Controlador no encontrado: " . $controller . "<br />";
                 }
-            } else {
-                echo "Ruta no encontrada en el enrutador: " . $url . "<br />";
             }
 
+            // Si no se encuentra la ruta
+            echo "Ruta no encontrada en el enrutador: " . $url . "<br />";
             echo "404 - Página no encontrada";
         }
 
